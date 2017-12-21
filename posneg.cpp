eeac7af41754e16ae8f0d7029cc2688b73ee30e7
abuthahirsidhiq <34731668+abuthahirsidhiq@users.noreply.github.com>
2017-12-21 10:09:41 +0530

    Create posneg.cpp

---
+++ posneg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    if(num>0)
+    cout<<"\npositive";
+    else if(num<0)
+    cout<<"\nnegative";
+    else 
+    cout<<"\nneutral";
+	return 0;
+}
